5860afbe03fbdf4a26357a8d6cb25602e0619304
Siddharth Jain <83913594+siddharthjain25@users.noreply.github.com>
2022-09-01 00:49:07 +0530

    Create color.h

---
+++ color.h
@@ -0,0 +1,178 @@
+//Author Siddharth Jain && Shubham Kumar
+//Shubham's Twitter: https://twitter.com/Maurya_05
+//Siddharth's Twitter: https://twitter.com/siddharth25op
+
+#include <stdio.h>
+//Yellow Color
+void yellow() 
+{
+    printf("\033[0;33m");
+}
+void yellowB() 
+{
+    printf("\033[1;33m");
+}
+void yellowD() 
+{
+    printf("\033[2;33m");
+}
+void yellowI() 
+{
+    printf("\033[3;33m");
+}
+void yellowU() 
+{
+    printf("\033[4;33m");
+}
+//Green Color
+void green() 
+{
+    printf("\033[0;32m");
+}
+void greenB() 
+{
+    printf("\033[1;32m");
+}
+void greenD() 
+{
+    printf("\033[2;32m");
+}
+void greenI() 
+{
+    printf("\033[3;32m");
+}
+void greenU() 
+{
+    printf("\033[4;32m");
+}
+//Purple Color
+void purple() 
+{
+    printf("\033[0;35m");
+}
+void purpleB() 
+{
+    printf("\033[1;35m");
+}
+void purpleD() 
+{
+    printf("\033[2;35m");
+}
+void purpleI() 
+{
+    printf("\033[3;35m");
+}
+void purpleU() 
+{
+    printf("\033[4;35m");
+}
+//Cyan Color
+void cyan() 
+{
+    printf("\033[0;36m");
+}
+void cyanB() 
+{
+    printf("\033[1;36m");
+}
+void cyanD() 
+{
+    printf("\033[2;36m");
+}
+void cyanI() 
+{
+    printf("\033[3;36m");
+}
+void cyanU() 
+{
+    printf("\033[4;36m");
+}
+//Black Color
+void black() 
+{
+    printf("\033[0;30m");
+}
+void blackB() 
+{
+    printf("\033[1;30m");
+}
+void blackD() 
+{
+    printf("\033[2;30m");
+}
+void blackI() 
+{
+    printf("\033[3;30m");
+}
+void blackU() 
+{
+    printf("\033[4;30m");
+}
+//White Color
+void white() 
+{
+    printf("\033[0;37m");
+}
+void whiteB() 
+{
+    printf("\033[1;37m");
+}
+void whiteD() 
+{
+    printf("\033[2;37m");
+}
+void whiteI() 
+{
+    printf("\033[3;37m");
+}
+void whiteU() 
+{
+    printf("\033[4;37m");
+}
+//Red Color
+void red()
+{
+    printf("\033[0;31m");
+}
+void redB()
+{
+    printf("\033[1;31m");
+}
+void redD()
+{
+    printf("\033[2;31m");
+}
+void redI()
+{
+    printf("\033[3;31m");
+}
+void redU()
+{
+    printf("\033[4;31m");
+}
+//Blue Color
+void blue()
+{
+    printf("\033[0;34m");
+}
+void blueB()
+{
+    printf("\033[1;34m");
+}
+void blueD()
+{
+    printf("\033[2;34m");
+}
+void blueI()
+{
+    printf("\033[3;34m");
+}
+void blueU()
+{
+    printf("\033[4;34m");
+}
+//Reset
+void reset()
+{
+    printf("\033[0m");
+}
