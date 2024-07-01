913c84036bf9f441ff8c8cc1001356b2b3a8c69a
Siddharth Jain <83913594+siddharthjain25@users.noreply.github.com>
2024-07-01 19:23:25 +0530

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,22 +1,22 @@
-#!/bin/bash
+#!/bin/sh
 
 # Function definitions
-function choice1 {
+choice1() {
     echo "You selected Choice 1"
     # Add the commands for Choice 1 here
 }
 
-function choice2 {
+choice2() {
     echo "You selected Choice 2"
     # Add the commands for Choice 2 here
 }
 
-function choice3 {
+choice3() {
     echo "You selected Choice 3"
     # Add the commands for Choice 3 here
 }
 
-function show_menu {
+show_menu() {
     echo "Please select an option:"
     echo "1. Choice 1"
     echo "2. Choice 2"
@@ -25,7 +25,8 @@ function show_menu {
 }
 
 # Main script
-while true; do
+while :
+do
     show_menu
     read -p "Enter your choice: " choice
     case $choice in
